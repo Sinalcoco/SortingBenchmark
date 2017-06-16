045de45d8733632cf721c4065af10cbe0cf927a3
PsiSchocker <psischocker@gmail.com>
2017-06-16 14:52:34 +0200

    Visualizer integration

--- src/de/dhbw/sort/SortingBenchmark.java
+++ src/de/dhbw/sort/SortingBenchmark.java
@@ -7,153 +7,196 @@ import de.dhbw.sort.util.AbstractAlgorithmHelper;
 import de.dhbw.sort.util.InPlaceAlgorithmHelper;
 import de.dhbw.sort.util.OutOfPlaceAlgorithmHelper;
 import de.dhbw.sort.util.Statistics;
+import de.dhbw.sort.visualize.*;
+import processing.core.PGraphics;
+import de.dhbw.sort.visualize.Visualizer;
 import processing.core.PApplet;
 
 public class SortingBenchmark extends PApplet {
 
-	public static void main(String[] args) {
-		PApplet.main("de.dhbw.sort.SortingBenchmark");
-	}
+    public static void main(String[] args) {
+        de.dhbw.sort.visualize.Visualizer visualizer = new Visualizer(1200,600,3,3);
+        PApplet.runSketch(new String[]{""},visualizer);
+        try {
+            // Es muss gewartet werden, bis PApplet die setup() Methode von
+            // Visualizer aufgerufen hat.
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        while (true) {
+            for (int i = 0; i < 4; i++) {
+                PGraphics s = visualizer.getScreen(i);
 
-	Statistics stats;
-	ArrayList<SortingAlgorithm> sorters;
+                s.beginDraw();
+                //Wähle irgendeine schöne Farbe
+                s.fill(i * 75 + 25,255 - 50 * i, i / 75 * 255);
+                s.rect(0, 0, s.width, s.height);
 
-	final int AMOUNT_OF_VALUES = 80;
-	final boolean ascending = true;
-	final boolean frameByFrame = false;
-	boolean advance = true;
-	int run = 20;
+                String text = Float.toString(visualizer.random(10));
+                //Der Text soll schwarz sein
+                s.fill(0);
+                //Die Positionierung ist einfach Pi mal Daumen.
+                s.text(text, s.width / 2 - text.length() * 3, s.height / 2);
 
-	public enum State {
-		WAIT, GO
-	}
+                s.endDraw();
+            }}
+    }
 
-	public void settings() {
-		size(1200, 600);
-		//fullScreen();
-	}
+    Statistics stats;
+    ArrayList<SortingAlgorithm> sorters;
 
-	public void setup() {
-		frameRate(50);
-		noStroke();
+    final int AMOUNT_OF_VALUES = 80;
+    final boolean ascending = true;
+    final boolean frameByFrame = false;
+    boolean advance = true;
+    int run = 20;
 
-		int[] values;
-		if (ascending) {
-			values = randomIntArray(run);
-		} else {
-			run = AMOUNT_OF_VALUES;
-			values = randomIntArray(run);
-			//values = sortedIntArray(run);
-			//values = invertedIntArray(run);
-		}
-		
-		stats = new Statistics();
-		
-		sorters = new ArrayList<SortingAlgorithm>();
+    public enum State {
+        WAIT, GO
+    }
 
-		AbstractAlgorithmHelper bubbleHelper = new InPlaceAlgorithmHelper(this, values, 0, 0, width / 3, height / 3);
-		sorters.add(new BubbleSort(bubbleHelper));
 
-		AbstractAlgorithmHelper cocktailHelper = new InPlaceAlgorithmHelper(this, values, width / 3, 0, width / 3,
-				height / 3);
-		sorters.add(new CocktailShaker(cocktailHelper));
+    public void setup() {
+        frameRate(50);
+        noStroke();
 
-		AbstractAlgorithmHelper insertionHelper = new InPlaceAlgorithmHelper(this, values, 2 * width / 3, 0, width / 3,
-				height / 3);
-		sorters.add(new InsertionSort(insertionHelper));
+        int[] values;
+        if (ascending)
+            {
+                values = randomIntArray(run);
+            } else
+            {
+                run = AMOUNT_OF_VALUES;
+                values = randomIntArray(run);
+                //values = sortedIntArray(run);
+                //values = invertedIntArray(run);
+            }
 
-		AbstractAlgorithmHelper selectionHelper = new InPlaceAlgorithmHelper(this, values, 0, height / 3, width / 3,
-				height / 3);
-		sorters.add(new SelectionSort(selectionHelper));
+        stats = new Statistics();
 
-		OutOfPlaceAlgorithmHelper mergeHelper = new OutOfPlaceAlgorithmHelper(this, values, 0, 2 * height / 3, width / 3,
-				height / 3);
-		sorters.add(new MergeSort(mergeHelper));
+        sorters = new ArrayList<SortingAlgorithm>();
 
-		AbstractAlgorithmHelper heapHelper = new InPlaceAlgorithmHelper(this, values, 2 * width / 3, height / 3, width / 3,
-				height / 3);
-		sorters.add(new HeapSort(heapHelper));
+        AbstractAlgorithmHelper bubbleHelper = new InPlaceAlgorithmHelper(this, values, 0, 0, width / 3, height / 3);
+        sorters.add(new BubbleSort(bubbleHelper));
 
-		AbstractAlgorithmHelper quickHelper = new InPlaceAlgorithmHelper(this, values, width / 3, 2 * height / 3, width / 3,
-				height / 3);
-		sorters.add(new QuickSort(quickHelper));
+        AbstractAlgorithmHelper cocktailHelper = new InPlaceAlgorithmHelper(this, values, width / 3, 0, width / 3,
+                                                                            height / 3);
+        sorters.add(new CocktailShaker(cocktailHelper));
 
-		for (SortingAlgorithm s : sorters)
-			s.start();
-	}
+        AbstractAlgorithmHelper insertionHelper = new InPlaceAlgorithmHelper(this, values, 2 * width / 3, 0, width / 3,
+                                                                             height / 3);
+        sorters.add(new InsertionSort(insertionHelper));
 
-	public void keyPressed() {
-		advance = true;
-	}
+        AbstractAlgorithmHelper selectionHelper = new InPlaceAlgorithmHelper(this, values, 0, height / 3, width / 3,
+                                                                             height / 3);
+        sorters.add(new SelectionSort(selectionHelper));
 
-	public void draw() {
-		if (!frameByFrame || advance) {
-			advance = false;
-			background(0);
-			boolean allReady = true;
+        OutOfPlaceAlgorithmHelper mergeHelper = new OutOfPlaceAlgorithmHelper(this, values, 0, 2 * height / 3,
+                                                                              width / 3, height / 3);
+        sorters.add(new MergeSort(mergeHelper));
 
-			for (SortingAlgorithm s : sorters) {
+        AbstractAlgorithmHelper heapHelper = new InPlaceAlgorithmHelper(this, values, 2 * width / 3, height / 3,
+                                                                        width / 3, height / 3);
+        sorters.add(new HeapSort(heapHelper));
 
-				s.helper().processChange();
-				if (!s.helper().ready()) {
-					allReady = false;
-				}
-			}
-			if (allReady) {
-				for (SortingAlgorithm s : sorters) {
-					s.helper().setState(State.GO);
-				}
-			}
-			if (ascending && run < AMOUNT_OF_VALUES) {
-				boolean allDone = true;
-				for (SortingAlgorithm s : sorters) {
-					if (!s.done()) {
-						allDone = false;
-						break;
-					}
-				}
-				if (allDone) {
-					run++;
-					int[] values = randomIntArray(run);
-					for (int i = 0; i < sorters.size(); i++) {
-						AbstractAlgorithmHelper h = sorters.get(i).helper();
-						int operations = h.getMoves() + h.getComparisons();
-						stats.addData(run - 1, h.getName(), operations);
-						h.setNewArray(values);
-						sorters.get(i).reset();
-					}
-					stats.printStatistic(run-1);
-				}
-			}
-		}
-	}
+        AbstractAlgorithmHelper quickHelper = new InPlaceAlgorithmHelper(this, values, width / 3, 2 * height / 3,
+                                                                         width / 3, height / 3);
+        sorters.add(new QuickSort(quickHelper));
 
-	public int[] invertedIntArray(int amountOfInts) {
-		int[] array = new int[amountOfInts];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = array.length - i;
-		}
-		return array;
-	}
+        for (SortingAlgorithm s : sorters)
+            {
+                s.start();
+            }
+    }
 
-	public int[] sortedIntArray(int amountOfInts) {
-		int[] array = new int[amountOfInts];
-		for (int i = 1; i <= array.length; i++) {
-			array[i - 1] = i;
-		}
-		return array;
-	}
+    public void keyPressed() {
+        advance = true;
+    }
 
-	public int[] randomIntArray(int amountOfInts) {
-		int[] array = new int[amountOfInts];
-		for (int i = 1; i <= array.length; i++) {
-			int index = (int) random(array.length);
-			while (array[index] != 0) {
-				index = (int) random(array.length);
-			}
-			array[index] = i;
-		}
+    public void draw() {
+        if (!frameByFrame || advance)
+            {
+                advance = false;
+                background(0);
+                boolean allReady = true;
 
-		return array;
-	}
+                for (SortingAlgorithm s : sorters)
+                    {
+
+                        s.helper().processChange();
+                        if (!s.helper().ready())
+                            {
+                                allReady = false;
+                            }
+                    }
+                if (allReady)
+                    {
+                        for (SortingAlgorithm s : sorters)
+                            {
+                                s.helper().setState(State.GO);
+                            }
+                    }
+                if (ascending && run < AMOUNT_OF_VALUES)
+                    {
+                        boolean allDone = true;
+                        for (SortingAlgorithm s : sorters)
+                            {
+                                if (!s.done())
+                                    {
+                                        allDone = false;
+                                        break;
+                                    }
+                            }
+                        if (allDone)
+                            {
+                                run++;
+                                int[] values = randomIntArray(run);
+                                for (int i = 0; i < sorters.size(); i++)
+                                    {
+                                        AbstractAlgorithmHelper h = sorters.get(i).helper();
+                                        int operations = h.getMoves() + h.getComparisons();
+                                        stats.addData(run - 1, h.getName(), operations);
+                                        h.setNewArray(values);
+                                        sorters.get(i).reset();
+                                    }
+                                stats.printStatistic(run - 1);
+                            }
+                    }
+            }
+    }
+
+    public int[] invertedIntArray(int amountOfInts) {
+        int[] array = new int[amountOfInts];
+        for (int i = 0; i < array.length; i++)
+            {
+                array[i] = array.length - i;
+            }
+        return array;
+    }
+
+    public int[] sortedIntArray(int amountOfInts) {
+        int[] array = new int[amountOfInts];
+        for (int i = 1; i <= array.length; i++)
+            {
+                array[i - 1] = i;
+            }
+        return array;
+    }
+
+    public int[] randomIntArray(int amountOfInts) {
+        int[] array = new int[amountOfInts];
+        for (int i = 1; i <= array.length; i++)
+            {
+                int index = (int) random(array.length);
+                while (array[index] != 0)
+                    {
+                        index = (int) random(array.length);
+                    }
+                array[index] = i;
+            }
+
+        return array;
+    }
 }
--- src/de/dhbw/sort/visualize/Visualizer.java
+++ src/de/dhbw/sort/visualize/Visualizer.java
@@ -1,7 +1,113 @@
 package de.dhbw.sort.visualize;
 
+import de.dhbw.sort.util.AbstractAlgorithmHelper;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+
 /**
  * Created by jbi on 13.06.2017.
  */
-public class Visualizer {
+public class Visualizer extends PApplet {
+    int fullScreen;
+
+    private int width = 200;
+    private int height = 600;
+    private int grid1 = 3;
+    private int grid2 = 3;
+    private int fWidth;
+    private int fHeight;
+
+    private PGraphics[] gs;
+
+    public Visualizer(int width, int height, int grid1, int grid2) {
+        this.width = width;
+        this.height = height;
+        this.grid1 = grid1;
+        this.grid2 = grid2;
+
+        this.fWidth = width / grid1;
+        this.fHeight = height / grid2;
+
+        gs = new PGraphics[grid1 * grid2];
+        fullScreen = -1;
+
+    }
+    public PGraphics getScreen(int index) {
+        return gs[index];
+    }
+    public void addAlgorithemHelper(AbstractAlgorithmHelper helper) {
+    }
+
+
+    public void initScreens() {
+
+        for (int i = 0; i < gs.length; i++)
+            {
+                gs[i] = createGraphics(fWidth, fHeight);
+            }
+    }
+
+    public void setup() {
+        surface.setResizable(true);
+        surface.setSize(width, height);
+        initScreens();
+    }
+
+    public void settings() {
+        size(width, height);
+    }
+
+    public void mousePressed() {
+        //Ordne dem Mausclick ein Fenster zu und setzte es auf fullscreen.
+        if (fullScreen == -1)
+            {
+                if (mouseX < fWidth / 2)
+                    {
+                        if (mouseY < fHeight)
+                            {
+                                fullScreen = 0;
+                            } else
+                            {
+                                fullScreen = 2;
+                            }
+                    } else
+                    {
+                        if (mouseY < fHeight / 2)
+                            {
+                                fullScreen = 1;
+                            } else
+                            {
+                                fullScreen = 3;
+                            }
+                    }
+            } else
+            {
+                //War schon eins Vollbild setze die ansicht wieder zurück
+                fullScreen = -1;
+                initScreens();
+            }
+    }
+
+    public void draw() {
+        //Wenn keines im fullScreen Modus ist zeichne einfach alle normal
+        if (fullScreen == -1)
+            {
+                for (int i = 0; i < gs.length; i++)
+                    {
+                        image(gs[i], (i % grid2) * (fWidth), (i / grid1) * (fHeight));
+                    }
+            } else
+            {
+                //Eines wurde als fullScreen ausgewählt
+                //Ist es der erste Frame nach der Auswahl, erstelle eine fullscreen-Grafik
+                if (gs[fullScreen].width < width || gs[fullScreen].height < height)
+                    {
+                        gs[fullScreen] = createGraphics(width, height);
+                    } else
+                    {
+                        //In jedem Folgeframe zeichne nur das fullscreen fenster
+                        image(gs[fullScreen], 0, 0);
+                    }
+            }
+    }
 }
