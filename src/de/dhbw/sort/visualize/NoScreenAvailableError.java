52426cb5868f6b1f48b7e963bbb59f8e0f84b563
S. Jung <simonjung1603@gmail.com>
2017-07-17 21:43:05 +0200

    Forgot the Exception

---
+++ src/de/dhbw/sort/visualize/NoScreenAvailableError.java
@@ -0,0 +1,10 @@
+package de.dhbw.sort.visualize;
+
+public class NoScreenAvailableError extends Exception {
+	public NoScreenAvailableError() {
+	}
+
+	public NoScreenAvailableError(String message) {
+		super(message);
+	}
+}
